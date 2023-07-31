bc2025b1728b433bee4c2463ce1abfa33a833710
Shreya Parkar <shreya.parkar.197@gmail.com>
2023-08-01 00:02:18 +0530

    Completed before but uploading now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=51da484a";
+// 51da484a
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+    
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovie("Batman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>PVS Cinema</h1>
+
+      <div className="search">
+            <input
+                placeholder="search"
+                value= {searchTerm}
+                onChange={(e) => {setSearchTerm(e.target.value)}} />
+            <img
+                src={SearchIcon}
+                alt="search"
+                onClick={() => {searchMovie(searchTerm)}} />
+      </div>
+
+        {
+            movies?.length > 0 ? (
+                <div className="container"> {
+                    movies.map((movie) => (
+                        <MovieCard movie={movie}/>
+                    ))          
+                }   
+                </div>) : (
+                <div className="empty">
+                    <h3>No movies found</h3>
+                    <p>Try searching for a movie title</p>
+            </div>
+        )}
+
+      {/* <div className="container">
+        <MovieCard movie1={movie1} />
+      </div> */}
     </div>
   );
-}
-
+};
+// every created component needs to be exported as default
 export default App;
